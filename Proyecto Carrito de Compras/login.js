ad02ca577b61c953f6a478c578afbe8cc35d41fa
crisbox1 <crisfesanchg@unimonserrate.edu.co>
2025-03-13 10:55:23 -0500

    Add files via upload

---
+++ Proyecto Carrito de Compras/login.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.querySelector("form");
+
+    loginForm.addEventListener("submit", function (event) {
+        event.preventDefault(); // Evita que la página se recargue
+
+        const usuario = document.getElementById("usuario").value;
+        const password = document.getElementById("password").value;
+
+        // Usuarios permitidos
+        if ((usuario === "admin" && password === "admin") ||
+            (usuario === "estudiante" && password === "estudiante")) {
+            
+            localStorage.setItem("isLoggedIn", "true"); // Guarda la sesión
+            window.location.href = "index.html"; // Redirige a productos
+            
+        } else {
+            alert("Usuario o contraseña incorrectos");
+        }
+    });
+});
